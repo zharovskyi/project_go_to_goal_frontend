652818e8bee4ab8ec9daff4f16f0e0162a41acad
bor9sik <asdf514asdf514@gmail.com>
2019-09-17 17:27:16 +0300

    added header to dashboard

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from '../../components/Header/Header';
 
 class LoginPage extends Component {
   state = {};
@@ -7,7 +6,6 @@ class LoginPage extends Component {
   render() {
     return (
       <div>
-        <Header />
         <h2>LoginPage</h2>
       </div>
     );
