585a9739a35ca5be3e19a77960e16e7e08f4ac16
bor9sik <asdf514asdf514@gmail.com>
2019-09-17 16:48:55 +0300

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import css from './Header.module.css';
-import Logo from './Logo/Logo';
-import Goal from './Goal/Goal';
-import User from './User/User';
+// import Logo from './Logo/Logo';
+import Goal from '../Goal/Goal';
+// import User from './User/User  ';
 
 const Header = () => {
   return (
     <header className={css.header}>
-      {/* <Logo />
+      {/* <Logo /> */}
       <Goal />
-      <User /> */}
-      dasda
+      {/* <User /> */}
     </header>
   );
 };
