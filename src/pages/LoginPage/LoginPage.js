81a6cfbd3c52c9e4aac7abad1eabf6bcf642b712
Kh-Ol-An <olegkhristenko@gmail.com>
2019-09-17 18:52:17 +0300

    interim

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import s from './LoginPage.module.css';
 
 class LoginPage extends Component {
-  state = {};
+  state = {
+    login: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // const { login, password } = this.state;
+
+    // this.props.onSubmit({ ...this.state });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      login: '',
+      password: '',
+    });
+  };
 
   render() {
+    const { login, password } = this.state;
     return (
-      <div>
-        <h2>LoginPage</h2>
-      </div>
+      <body>
+        <header>
+          <img src="../../assets/images/login-page-logo@1X.png" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="login"
+              value={login}
+              onChange={this.handleChange}
+              placeholder="Enter your login"
+            />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+              placeholder="Enter your password"
+            />
+            <button type="submit">Login</button>
+            <button type="button">Registration</button>
+          </form>
+        </header>
+        <main>
+          <div className={s.cover}>
+            <img
+              src="../../assets/images/login-page-cover@1X.png"
+              alt="cover"
+            />
+          </div>
+          <div>greeting</div>
+        </main>
+        <footer>@ Copyright 2019</footer>
+      </body>
     );
   }
 }
