ff858d8021c3953b56c2afa6869e6e6e2e50b671
v-potap <v.o.potapenko@gmail.com>
2019-09-17 17:17:33 +0300

    draft 01

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//  COMPONENTS
+
+//  STYLES
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footerText}>
+        &#169; Copyright 2019
+        <span className={styles.footerCopyright}> BOOTC@MP#13 team</span>
+      </p>
+    </div>
+  );
+}
--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+// COMPONENTS
+import Header from '../../components/Header/Header';
+import TaskList from '../../components/TaskList/TaskList';
+import Footer from '../../components/Footer/Footer';
+import ModalCreateGoal from '../../components/ModalCreateGoal/ModalCreateGoal';
+import ModalAddTask from '../../components/ModalAddTask/ModalAddTask';
+import ModalLogout from '../../components/ModalLogout/ModalLogout';
+import ModalDeleteTask from '../../components/ModalDeleteTask/ModalDeleteTask';
+import ModalCongrats from '../../components/ModalCongrats/ModalCongrats';
+
 const DashboardPage = () => {
   return (
     <div>
-      <ModalRegistration />
+      {/* <Header />
+      <TaskList /> */}
+      <Footer />
+      {/* <ModalCreateGoal />
+      <ModalAddTask />
+      <ModalLogout />
+      <ModalDeleteTask />
+      <ModalCongrats /> */}
     </div>
   );
 };
