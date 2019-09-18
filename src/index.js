229e15909d92a631d74f921faf18928266a73291
OlgaSavytska <o.savytska77@gmail.com>
2019-09-18 17:42:18 +0300

    init ModalAddTask

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-// import './stylesheet/main.css';
+import './stylesheet/main.css';
 import App from './components/App';
 
 ReactDOM.render(
