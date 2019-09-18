9ddb855f4cdef0d766fa69d43e267f9b5f2d203f
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-18 15:55:22 +0300

    deleted ModalCreateGoal from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
-import ModalCreateGoal from './ModalCreateGoal/ModalCreateGoal';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" exact component={LoginPage} />
         <Route path="/dashboard" component={DashboardPage} />
       </Switch>
-      <ModalCreateGoal />
     </>
   );
 }
