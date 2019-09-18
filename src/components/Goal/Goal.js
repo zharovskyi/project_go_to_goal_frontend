29baf660592ec3dd26e5ef05cd9c63a5ec6203ef
bor9sik <asdf514asdf514@gmail.com>
2019-09-18 12:40:05 +0300

    f***

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Progress } from 'react-sweet-progress';
 import 'react-sweet-progress/lib/style.css';
 import css from './Goal.module.css';
 
@@ -9,31 +8,6 @@ const Goal = () => {
       <div className={css.goalLogo}>
         <p className={css.goalName}> Mоя мета:</p>
         <div className={css.goalLabel}>Вeлосипед</div>
-      </div>
-      <div className={css.progresBar}>
-        <p className={css.progresScore}>
-          999 / <span className={css.total}> 1000</span>
-        </p>
-        {/* <div className={css.progresBar} /> */}
-        <Progress
-          className={css.progresBar}
-          percent={(500 / 1000) * 100}
-          symbol="none"
-          theme={{
-            success: {
-              symbol: '😀',
-              color: '#9ecb45',
-            },
-            active: {
-              symbol: ' ',
-              color: '#f1c540',
-            },
-            default: {
-              symbol: ' ',
-              color: '#fbc630',
-            },
-          }}
-        />
       </div>
     </div>
   );
