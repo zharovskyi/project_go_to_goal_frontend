ccbf9c8bd03159b5b76709ca7df369e597295757
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-18 15:53:47 +0300

    added shortid and changed ModalCreateGoal.js

--- src/components/ModalCreateGoal/ModalCreateGoal.js
+++ src/components/ModalCreateGoal/ModalCreateGoal.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
 import s from './ModalCreateGoal.module.css';
+
+const idForInputGoal = shortid.generate();
+const idForInputPoints = shortid.generate();
 
 class ModalCreateGoal extends Component {
   state = {};
 
   render() {
     return (
-      <div className={s.overlay}>
-        <div className={s.modal}>
-          <form className={s.form}>
-            <label className={s.label}>
-              Що я хочу
-              <input
-                className={s.input}
-                name="valueInputSurname"
-                type="text"
-                placeholder="Дай своїй меті назву"
-              />
-            </label>
-            <label className={s.label}>
-              Скільки балів треба набрати
-              <input
-                className={s.input}
-                name="access"
-                type="text"
-                placeholder="Напиши при якій кількості балів мета буде вважатися досягнутою (напр.: 1000)"
-              />
-            </label>
-            <textarea
-              className={s.textarea}
-              name="description"
-              placeholder="Напиши, що тебе мотивує:"
+      <div className={s.modal}>
+        <form className={s.form}>
+          <label htmlFor={idForInputGoal} className={s.label}>
+            Що я хочу
+            <input
+              id={idForInputGoal}
+              className={s.input}
+              name="valueInputSurname"
+              type="text"
+              placeholder="Дай своїй меті назву"
             />
-            <button className={s.button} type="button">
-              Ok
-            </button>
-          </form>
-        </div>
+          </label>
+          <label htmlFor={idForInputPoints} className={s.label}>
+            Скільки балів треба набрати
+            <input
+              id={idForInputPoints}
+              className={s.input}
+              name="access"
+              type="text"
+              placeholder="Наприклад: 1000"
+            />
+          </label>
+          <textarea
+            className={s.textarea}
+            name="description"
+            placeholder="Напиши, що тебе мотивує:"
+          />
+          <button className={s.button} type="button">
+            Ok
+          </button>
+        </form>
       </div>
     );
   }
