478c167fd39e4d179b25a38689d5f0a129e5cab4
Dmitriy <velkov55@gmail.com>
2019-09-19 11:53:54 +0300

    file editing CreateTaskButton

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
+import CreateTaskButton from './CreateTaskButton/CreateTaskButton';
 
 function App() {
   return (
     <>
+      <CreateTaskButton />
       <Switch>
         <Route path="/" exact component={LoginPage} />
         <Route path="/dashboard" component={DashboardPage} />
--- src/components/CreateTaskButton/CreateTaskButton.js
+++ src/components/CreateTaskButton/CreateTaskButton.js
@@ -3,11 +3,11 @@ import styles from './CreateTaskButton.module.css';
 
 const CreateTaskButton = () => {
   return (
-    <div>
+    <>
       <button type="button" className={styles.button}>
         &#x2b;
       </button>
-    </div>
+    </>
   );
 };
 
