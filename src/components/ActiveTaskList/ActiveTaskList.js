3d4ec16e06e1fa72f0d39171d632b86013863206
Andrii Kuznietsov <awesomeakm@gmail.com>
2019-09-19 16:03:39 +0300

    completed few tasks

--- src/components/ActiveTaskList/ActiveTaskList.js
+++ src/components/ActiveTaskList/ActiveTaskList.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import styles from './ActiveTaskList.module.css';
 import Card from '../Card/Card';
+import * as TaskListSelectors from '../../redux/TaskList/TaskListSelectors';
 
 class ActiveTaskList extends Component {
   state = {};
 
   render() {
+    const { activePosts, donePosts } = this.props;
+    console.log(activePosts);
+    console.log(donePosts);
+
     return (
       <>
         <ul className={styles.activeCards}>
           <Card />
+          {/* <Card />
           <Card />
           <Card />
           <Card />
           <Card />
-          <Card />
-          <Card />
+          <Card /> */}
         </ul>
       </>
     );
   }
 }
 
-export default ActiveTaskList;
+ActiveTaskList.propTypes = {
+  activePosts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  donePosts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+const mapStateToProps = store => ({
+  activePosts: TaskListSelectors.getActivePosts(store),
+  donePosts: TaskListSelectors.getDonePosts(store),
+});
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ActiveTaskList);
