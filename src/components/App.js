75a41b2833dc3ce4d93af8e8b017bd363e5c397d
OlgaSavytska <o.savytska77@gmail.com>
2019-09-20 13:21:10 +0300

    init ModalAddTask

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import DashboardContainer from '../pages/DashboardPage/DashboardContainer';
-import Modal from './ModalAddTask/ModalAddTask';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" exact component={LoginPage} />
         <Route path="/dashboard" component={DashboardContainer} />
       </Switch>
-      <Modal />
     </>
   );
 }
