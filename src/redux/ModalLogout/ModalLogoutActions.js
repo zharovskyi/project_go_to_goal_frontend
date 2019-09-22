de564d10e293719e2306ed43023b2ea2736b017d
bor9sik <asdf514asdf514@gmail.com>
2019-09-22 17:11:47 +0300

    added logic on logout

--- src/redux/ModalLogout/ModalLogoutActions.js
+++ src/redux/ModalLogout/ModalLogoutActions.js
@@ -1,10 +1,27 @@
 export const Type = {
   MODAL_LOGOUT_OPEN: 'MODAL_LOGOUT_OPEN',
   MODAL_LOGOUT_CLOSE: 'MODAL_LOGOUT_CLOSE',
+  LOG_OUT_START: 'LOG_OUT_START',
+  LOG_OUT_SUCCSES: 'LOG_OUT_SUCCSES',
+  LOG_OUT_ERROR: 'LOG_OUT_ERROR',
 };
-export const openModal = () => ({
+
+export const openModal = e => ({
   type: Type.MODAL_LOGOUT_OPEN,
+  isModalLogoutOpen: true,
 });
-export const closeModal = () => ({
+
+export const closeModal = e => ({
   type: Type.MODAL_LOGOUT_CLOSE,
+  isModalLogoutOpen: false,
 });
+
+export const logOutSucsses = session => ({
+  type: Type.LOG_OUT_SUCCSES,
+});
+export const logOutError = error => ({
+  type: Type.LOG_OUT_ERROR,
+  payload: error,
+});
+
+//  ADd action to reducer
