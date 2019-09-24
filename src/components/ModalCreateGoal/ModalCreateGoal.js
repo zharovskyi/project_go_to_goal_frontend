d7a706d298b3cfc09dd33c68975192b1686e9a3b
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-24 11:28:21 +0300

    changed ModalCreateGoalLogic.module.css

--- src/components/ModalCreateGoal/ModalCreateGoal.js
+++ src/components/ModalCreateGoal/ModalCreateGoal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import { connect } from 'react-redux';
-import s from './ModalCreateGoal.module.css';
+import styles from './ModalCreateGoal.module.css';
 import { addGoal } from '../../redux/ModalCreateGoal/ModalCreateGoalOperations';
 import * as dashboardSelectors from '../../redux/Dashboard/DashboardSelectors';
 
@@ -14,7 +14,6 @@ class ModalCreateGoal extends Component {
   state = {
     valueInputGoal: '',
     valueInputPoints: '',
-    valueDescriptionArea: '',
   };
 
   handleChange = e => {
@@ -26,17 +25,13 @@ class ModalCreateGoal extends Component {
   onSubmitForm = async e => {
     e.preventDefault();
     const { postGoal, token } = this.props;
-    const {
-      valueInputGoal,
-      valueInputPoints,
-      valueDescriptionArea,
-    } = this.state;
+    const { valueInputGoal, valueInputPoints } = this.state;
 
     await postGoal(
       {
         title: valueInputGoal,
         points: Number(valueInputPoints),
-        description: valueDescriptionArea,
+        description: 'goal description',
         dates: [date],
       },
       token,
@@ -45,62 +40,54 @@ class ModalCreateGoal extends Component {
     await this.setState({
       valueInputGoal: '',
       valueInputPoints: '',
-      valueDescriptionArea: '',
     });
   };
 
   render() {
-    const {
-      valueInputGoal,
-      valueInputPoints,
-      valueDescriptionArea,
-    } = this.state;
+    const { valueInputGoal, valueInputPoints } = this.state;
     return (
-      <div className={s.modal}>
-        <form onSubmit={this.onSubmitForm} className={s.form}>
-          <label htmlFor={idForInputGoal} className={s.label}>
-            Що я хочу
-            <input
-              minLength="3"
-              maxLength="20"
-              name="valueInputGoal"
-              value={valueInputGoal}
-              onChange={this.handleChange}
-              id={idForInputGoal}
-              className={s.input}
-              type="text"
-              placeholder="Дай своїй цілі назву"
-              required
-            />
-          </label>
-          <label htmlFor={idForInputPoints} className={s.label}>
-            Скільки балів треба набрати
-            <input
-              min="1"
-              max="9999"
-              name="valueInputPoints"
-              value={valueInputPoints}
-              onChange={this.handleChange}
-              id={idForInputPoints}
-              className={s.input}
-              type="number"
-              placeholder="Наприклад: 1000"
-              required
-            />
-          </label>
-          <textarea
-            min="5"
-            name="valueDescriptionArea"
-            value={valueDescriptionArea}
-            onChange={this.handleChange}
-            className={s.textarea}
-            placeholder="Напиши, що тебе мотивує:"
-            required
-          />
-          <button className={s.button} type="submit">
-            Ok
-          </button>
-        </form>
+      <div className={styles.overlay}>
+        <div className={styles.modal}>
+          <form onSubmit={this.onSubmitForm} className={styles.form}>
+            <label htmlFor={idForInputGoal} className={styles.label}>
+              Що я хочу
+              <input
+                minLength="3"
+                maxLength="20"
+                name="valueInputGoal"
+                value={valueInputGoal}
+                onChange={this.handleChange}
+                id={idForInputGoal}
+                className={styles.input}
+                type="text"
+                placeholder="Дай своїй цілі назву"
+                required
+              />
+            </label>
+            <label htmlFor={idForInputPoints} className={styles.label}>
+              Скільки балів треба набрати
+              <input
+                min="1"
+                max="9999"
+                name="valueInputPoints"
+                value={valueInputPoints}
+                onChange={this.handleChange}
+                id={idForInputPoints}
+                className={styles.input}
+                type="number"
+                placeholder="Наприклад: 1000"
+                required
+              />
+            </label>
+            <p className={styles.text}>
+              Дай цілі назву та вибери при якій кількості балів ціль буде
+              вважатися досягнутою
+            </p>
+            <button className={styles.button} type="submit">
+              Ok
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
