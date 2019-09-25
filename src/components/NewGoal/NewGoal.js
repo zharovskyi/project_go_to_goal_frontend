f467ef3d3c20d624ae680a67a2ae79e25b3118b5
bor9sik <asdf514asdf514@gmail.com>
2019-09-25 14:50:15 +0300

    text fixed

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -8,7 +8,7 @@ import * as ModalCreateGoalActions from '../../redux/ModalCreateGoal/ModalCreate
 const NewGoal = ({ openModal }) => {
   return (
     <div className={styles.new_goal_section}>
-      <h2 className={styles.new_goal_title}>Моя мета</h2>
+      <h2 className={styles.new_goal_title}>Моя ціль</h2>
       <div className={styles.container}>
         <button type="button" className={styles.button} onClick={openModal}>
           <div className={styles.button_overlay}>
@@ -16,11 +16,11 @@ const NewGoal = ({ openModal }) => {
               <AddPlusButton className={styles.icon} />
             </div>
           </div>
-          <p className={styles.button_text}> Додай мету</p>
+          <p className={styles.button_text}> Додай ціль</p>
         </button>
         <div className={styles.new_goal_info}>
           <p className={styles.p}>
-            Cпочатку вам треба узгодити мету, після чого можна буде слідкувати
+            Cпочатку вам треба узгодити ціль, після чого можна буде слідкувати
             за прогресом виконання.
           </p>
           <p className={styles.p}>Подобається ідея?</p>
