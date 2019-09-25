fcd1df73ef062ff4328514719ffd26d93137c6d9
bor9sik <asdf514asdf514@gmail.com>
2019-09-25 14:19:15 +0300

    done tast list

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from './TaskList.module.css';
+
 import ActiveTaskList from '../ActiveTaskList/ActiveTaskList';
 import DoneTaskList from '../DoneTaskList/DoneTaskList';
 import CreateTaskButton from '../CreateTaskButton/CreateTaskButton';
+import * as dashboardSelectors from '../../redux/Dashboard/DashboardSelectors';
 
-const TaskList = (activePosts, getGoal) => {
+const TaskList = (activePosts, getGoal, goal) => {
   return (
     <div className={styles.taskList}>
       <CreateTaskButton />
       <ActiveTaskList activePosts={activePosts} getGoal={getGoal} />
-      <DoneTaskList />
+      {goal === null && <DoneTaskList />}
     </div>
   );
 };
+const MSTP = s => ({
+  goal: dashboardSelectors.getGoal(s),
+});
 
-export default TaskList;
+export default connect(
+  MSTP,
+  null,
+)(TaskList);
